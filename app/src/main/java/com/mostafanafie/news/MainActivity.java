91551fe8944edfe59101a10cc5766c8053545aa5
Mostafa Nafie <MostafaTarekNafie@gmail.com>
2019-07-17 21:26:36 +0200

    Update internet connection check logic

--- app/src/main/java/com/mostafanafie/news/MainActivity.java
+++ app/src/main/java/com/mostafanafie/news/MainActivity.java
@@ -6,7 +6,6 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -32,12 +31,10 @@ public class MainActivity extends AppCompatActivity
     private Context mContext = this;
     // Tag for log messages
     private String TAG = MainActivity.class.getSimpleName();
-
     // URL for news data from the Guardian
     private static final String GUARDIAN_REQUEST_URL = "https://content.guardianapis.com/search";
     // Constant value for the news loader ID
     private static final int NEWS_LOADER_ID = 1;
-
     // Initialize the RecyclerView
     @BindView(R.id.recycler_view)
     RecyclerView recyclerView;
@@ -55,26 +52,8 @@ public class MainActivity extends AppCompatActivity
 
         // Initialize ButterKnife
         ButterKnife.bind(this);
-
-        // Get a reference to the ConnectivityManager to check state of network connectivity
-        ConnectivityManager connMgr = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        // Get details on the currently active default data network
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-
-        // If there is a network connection, fetch data
-        if (networkInfo != null && networkInfo.isConnected()) {
-            // Initialize the LoaderManager
-            LoaderManager loaderManager = getLoaderManager();
-            // Initialize the loader
-            loaderManager.initLoader(NEWS_LOADER_ID, null, this);
-        } else {
-            // Update empty state with no connection error message
-            loadingIndicator.setVisibility(View.GONE);
-            emptyStateTextView.setText(R.string.no_internet_connection);
-        }
-
+        // A method to check the Internet connection
+        checkConnection();
     }
 
     @NonNull
@@ -109,7 +88,6 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onLoaderReset(android.content.Loader<List<News>> loader) {
-
     }
 
     private void setupRecyclerView(ArrayList<News> newsList) {
@@ -119,5 +97,26 @@ public class MainActivity extends AppCompatActivity
         recyclerView.setAdapter(adapter);
         // Attach the layout manager to the recycler view
         recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+    }
+
+    private void checkConnection() {
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Initialize the LoaderManager
+            LoaderManager loaderManager = getLoaderManager();
+            // Initialize the loader
+            loaderManager.initLoader(NEWS_LOADER_ID, null, this);
+        } else {
+            // Update empty state with no connection error message
+            loadingIndicator.setVisibility(View.GONE);
+            emptyStateTextView.setText(R.string.no_internet_connection);
+        }
     }
 }
